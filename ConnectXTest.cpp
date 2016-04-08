7572028d1b1842e5c3bc1c43953d854c7362e633
september <gyelwande@mail.csuchico.edu>
2016-04-07 18:02:38 -0700

    files commited for quiz 3

--- ConnectXTest.cpp
+++ ConnectXTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "ConnectX.h"
- 
+
 class ConnectXTest : public ::testing::Test
 {
 	protected:
@@ -15,7 +15,46 @@ class ConnectXTest : public ::testing::Test
 		virtual void TearDown(){}
 };
 
+
 TEST(ConnectXTest, sanityCheck)
 {
-	ASSERT_TRUE(true);
+	 ASSERT_TRUE(true);
+}
+
+//It tests that values provided for width and height are not valid
+TEST(ConnectXTest, outOfBounds)
+{
+  ConnectX connectObj;
+  ASSERT_EQ(-1,connectObj.at(20,45));
+}
+
+//It tests that the values provided for at() fucntion are within the
+// range of width and height
+TEST(ConnectXTest, inBounds)
+{
+  ConnectX connectObj;
+  ASSERT_EQ(0,connectObj.at(2,3));
+}
+
+//It test that the location provided for piece is valid location
+TEST(ConnectXTest, piecePlacedAtValidLocation)
+{
+  ConnectX connectObj;
+  connectObj.placePiece(5);
+  ASSERT_EQ(2,connectObj.at(5,5));
+}
+
+//This test is used to check whose turn is next
+TEST(ConnectXTest, checkWhoseTurn)
+{
+  ConnectX connectObj;
+  ASSERT_EQ(2,connectObj.whoseTurn());
+}
+
+//It test that when passing values to constructor it chnages the board
+//structure accordingly
+TEST(ConnectXTest, checkForConstructor)
+{
+  ConnectX connectObj(-1,8,8);
+  connectObj.showBoard();
 }
